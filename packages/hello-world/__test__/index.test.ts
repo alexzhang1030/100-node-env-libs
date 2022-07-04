a0d530e5cb2b74ad1ad65783a8ed461846cc1881
alexzhang1030 <1642114555@qq.com>
2022-07-04 09:54:09 +0800

    chore: migrate to pnpm-monorepo

---
+++ packages/hello-world/__test__/index.test.ts
@@ -0,0 +1,5 @@
+import { hello } from '../index'
+
+test('hello world', () => {
+  expect(hello).toBe('world')
+})
--- test/index.test.ts
+++
@@ -1,5 +0,0 @@
-describe('developer-plus', () => {
-  it('happy path', () => {
-    expect(1).toBe(1)
-  })
-})
